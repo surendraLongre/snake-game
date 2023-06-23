87413166f1b5430c07dadb98d01eeff7f38ac360
surendraLongre <murari.longre2366@gmail.com>
2023-06-23 23:27:51 +0530

    release v1.0.0
    finish the snake game

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
 
 snake=Snake()
-snake.add_segment()
+food=Food()
+score=ScoreBoard()
 
 #set screen settings
 screen=Screen()
@@ -33,9 +37,30 @@ while snake.play:
         snake.move_snake(i)
 
     snake.arr[0].forward(20)
+    #detech colision with food
+    if snake.arr[0].distance(food)<20:
+        snake.add_segment()
+        food.pos()
+        score.inc_score()
 
+    #detech collision with the wall
+    if abs(int(snake.arr[0].xcor()))>580 or abs(int(snake.arr[0].ycor()))>580:
+        print("collided with the wall")
+        snake.play=False
+        score.game_over()
+
+    #detech collision with the tail
+
+    for segments in snake.arr:
+        if segments==snake.arr[0]:
+            pass
+        elif segments.distance(snake.arr[0])<10:
+            snake.play=False
+            score.game_over()
+            print("collided with its own tail")
 
 #endof snake game
 
+print("done")
 screen=Screen()
 screen.exitonclick()
